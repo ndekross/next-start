d3f35f9112abcc11c5e68fdf5700555602391ccf
Natanael de Kross <natanael@dekross.com>
2021-03-17 08:13:49 -0400

    Add typescript support to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,16 @@
-//TODO Configure webpack for global CSS Import
 module.exports = {
   reactOptions: {
     fastRefresh: true,
     strictMode: true,
   },
+  typescript: {
+    check: true,
+    reactDocgen: "react-docgen-typescript",
+  },
   stories: ["../stories/*.stories.@(js|jsx|ts|tsx|mdx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-dark-mode",
+  ],
 }
